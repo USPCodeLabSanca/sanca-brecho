c2a2a8ec95afda9dc9d6d38b8ddcc9acf16d9e62
pradoscript <prado.joao06480@gmail.com>
2025-04-20 15:39:17 -0300

    adjusting link's tags from nextjs

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -60,15 +60,15 @@ export default function RootLayout({
                 O marketplace universitário de São Carlos. Compre e venda produtos usados de forma fácil e rápida.
                 </p>
                 <div className="flex space-x-4">
-                <a href="#" className="text-gray-500 hover:text-sanca">
+                <Link href="#" className="text-gray-500 hover:text-sanca">
                   <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-facebook"><path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path></svg>
-                </a>
-                <a href="#" className="text-gray-500 hover:text-sanca">
+                </Link>
+                <Link href="#" className="text-gray-500 hover:text-sanca">
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-instagram"><rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path><line x1="17.5" x2="17.51" y1="6.5" y2="6.5"></line></svg>
-                </a>
-                <a href="#" className="text-gray-500 hover:text-sanca">
+                </Link>
+                <Link href="#" className="text-gray-500 hover:text-sanca">
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-twitter"><path d="M22 4s-.7 2.1-2 3.4c1.6 10-9.4 17.3-18 11.6 2.2.1 4.4-.6 6-2C3 15.5.5 9.6 3 5c2.2 2.6 5.6 4.1 9 4-.9-4.2 4-6.6 7-3.8 1.1 0 3-1.2 3-1.2z"></path></svg>
-                </a>
+                </Link>
                 </div>
               </div>
 
