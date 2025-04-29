5344735c8b51d428085af656c59eacdada612749
shogoshima <shogoshima@usp.br>
2025-04-29 14:07:08 -0300

    adding docker to backend

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"api/controllers"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-    fmt.Println("Hello World!")
+	router := gin.Default()
+	router.GET("/", controllers.HelloWorld)
+	router.Run(":8080")
 }
