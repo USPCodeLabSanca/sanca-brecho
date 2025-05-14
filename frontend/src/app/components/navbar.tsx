9cbbd0e835250c4660563b4af410836eaf35f6f9
Otávio Biagioni <otaviobiagioni31@gmail.com>
2025-05-14 13:20:07 -0300

    ✨feat: Página de Usuário
    - Adicionada página de usuário (acessível pelo path "usuario/[id]")
    - Alterado path de usuário em algumas páginas (anteriormente estava profile ou perfil, possivelmente podemos mudar pra um desses)
    - Corrigida lógica server/cliente da página de produto

--- frontend/src/app/components/navbar.tsx
+++ frontend/src/app/components/navbar.tsx
@@ -22,7 +22,7 @@ export default function Navbar() {
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-bell h-5 w-5 text-gray-600 hover:text-sanca"><path d="M6 8a6 6 0 0 1 12 0c0 7 3 9 3 9H3s3-2 3-9"></path><path d="M10.3 21a1.94 1.94 0 0 0 3.4 0"></path></svg>
               {/* span para quantidade de notificiações ajustar assim que backend ficar pronto */}
               </Link>
-              <Link href="/profile">
+              <Link href="/usuario/1">
                 <span className="relative flex shrink-0 overflow-hidden rounded-full h-8 w-8">
                   <Image alt="foto de perfil" width={225} height={225} className="aspect-square h-full w-full" src="https://i.pravatar.cc/150?img=1"/>
                 </span>
