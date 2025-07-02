4dddf3c42873f773b6cfb67e0fa238fe82183bb7
Gabriel Barbosa <50121055+GotemBarbosa@users.noreply.github.com>
2025-07-02 16:28:47 -0300

    comentario authService

--- frontend/src/lib/services/authService.ts
+++ frontend/src/lib/services/authService.ts
@@ -1,6 +1,8 @@
 import api from "../api/axiosConfig";
 import { UserType } from "../types/api";
 
+// O idToken é um JWT gerado pelo Firebase que contém informações do usuário autenticado.
+
 export const login = async(idToken: string): Promise<UserType> => {
     const response = await api.post('/login', {}, {
         headers: {
