e4c99a46acca04d011f419116bfd17b1c62f9dae
shogoshima <shogoshima@usp.br>
2025-07-15 21:45:48 -0300

    fix: fixing services and adding idToken to axios config

--- frontend/src/lib/services/userService.ts
+++ frontend/src/lib/services/userService.ts
@@ -6,13 +6,13 @@ import { UserType } from "../types/api";
 // buscar informacao do usuario logado
 export const getMe = async (): Promise<UserType> => {
     const response = await api.get("/users/me");
-    return response.data;
+    return response.data.user;
 }
 
 // Atualizar informações do usuário logado
 export const updateMe = async (updates: Partial<UserType>): Promise<UserType> => {
     const response = await api.put('/users/me', updates);
-    return response.data;
+    return response.data.user;
 };
 
 // Deletar a conta do usuário logado
